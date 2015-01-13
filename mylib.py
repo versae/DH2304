193544f136f4cf719070d940a2f45e7e280f42f1
Javier de la Rosa <versae@gmail.com>
2015-01-13 18:03:09 -0500

    Update classes 3 and 4

---
+++ mylib.py
@@ -0,0 +1,14 @@
+def maximum(a, b):
+    if a > b:
+        result = a
+    else:
+        result = b
+    return result
+
+
+def minimum(a, b):
+    if b < a:
+        result = b
+    else:
+        result = a
+    return result
